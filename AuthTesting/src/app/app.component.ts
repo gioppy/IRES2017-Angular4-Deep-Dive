54c01603bd889052494f8c2018d856fc75ea754b
Giovanni Buffa <tech@giovannibuffa.it>
2018-02-12 12:44:36 +0100

    Aggiunta applicazione per testare l'autenticazione via JWT

---
+++ AuthTesting/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor() { }
+
+  login() {
+    // Login user
+  }
+
+  logout() {
+    // Logout user
+  }
+
+  authRequest() {
+    // Make a request
+  }
+}
