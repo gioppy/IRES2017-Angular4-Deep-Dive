d002ce787159ec0ae1a78f6c132690ce077ac883
Giovanni Buffa <giova.buffa@gmail.com>
2018-02-13 09:41:59 +0100

    Update home.component.ts

--- RxPlayground/src/app/home/home.component.ts
+++ RxPlayground/src/app/home/home.component.ts
@@ -1,22 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/count';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/operator/last';
-import 'rxjs/add/operator/scan';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/bufferWhen';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/toArray';
-
-import { fakeData } from '../fake-data';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
